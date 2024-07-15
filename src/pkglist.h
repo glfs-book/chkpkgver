8afb963eba9e01795ee3a3fae225a5e34b0f5adb
Zeckmathederg <zeckmaisreal@gmail.com>
2024-07-14 21:03:47 -0600

    Set up to track

--- src/pkglist.h
+++ src/pkglist.h
@@ -6,46 +6,62 @@
 // char pkg_PackageName[4][100] = { "PackageName",
 // "<the name of the corrosponding ENTITY in the packages.ent file>",
 // "<the link of where to search for the new version of the package>",
-// "<The link to the release text/info of the package (making it NULL is okay)>"
+// "<The link to the release text/info of the package (making it "\0" is okay)>"
 // };
 
 char pkg_libtasn1[4][100] = { "libtasn1", "libtasn1-version",
 	"https://ftp.gnu.org/gnu/libtasn1/",
-	NULL
+	"\0"
 };
 char pkg_NSPR[4][100] = { "NSPR", "nspr-version",
 	"https://archive.mozilla.org/pub/nspr/releases/",
-	NULL
+	"\0"
 };
 char pkg_NSS[4][100] = { "NSS", "nss-dir",
 	"https://archive.mozilla.org/pub/security/nss/releases/",
-	NULL
+	"\0"
 };
-char pkg_p11-kit[4][100] = { "p11-kit", "p11-kit-version",
+char pkg_p11_kit[4][100] = { "p11-kit", "p11-kit-version",
 	"https://api.github.com/repos/p11-glue/p11-kit/releases/latest",
-	NULL
+	"\0"
 };
-char pkg_make-ca[4][100] = { "make-ca", "make-ca-version",
+char pkg_make_ca[4][100] = { "make-ca", "make-ca-version",
 	"https://api.github.com/repos/lfs-book/make-ca/releases/latest",
-	NULL
+	"\0"
 };
 char pkg_libunistring[4][100] = { "libunistring", "libunistring-version",
 	"https://ftp.gnu.org/gnu/libunistring/",
-	NULL
+	"\0"
 };
 char pkg_libidn2[4][100] = { "libidn2", "libidn2-version",
 	"https://ftp.gnu.org/gnu/libidn/",
-	NULL
+	"\0"
 };
 char pkg_libpsl[4][100] = { "libpsl", "libpsl-version",
 	"https://api.github.com/repos/rockdaboot/libpsl/releases/latest",
-	NULL
+	"\0"
 };
 char pkg_cURL[4][100] = { "cURL", "curl-version",
 	"https://curl.se/download/",
-	NULL
+	"\0"
 };
 char pkg_Wget[4][100] = { "Wget", "wget-version",
 	"https://ftp.gnu.org/gnu/wget/",
-	NULL
+	"\0"
+};
+char pkg_git[4][100] = { "git", "git-version",
+	"https://www.kernel.org/pub/software/scm/git/",
+	"\0"
+};
+char pkg_alsa_lib[4][100] = { "alsa-lib", "alsa-lib-version",
+	"https://www.alsa-project.org/files/pub/lib/",
+	"\0"
+};
+char pkg_alsa_plugins[4][100] = { "alsa-plugins", "alsa-plugins-version",
+	"https://www.alsa-project.org/files/pub/plugins/",
+	"\0"
+};
+char pkg_alsa_utils[4][100] = { "alsa-utils", "alsa-utils-version",
+	"https://www.alsa-project.org/files/pub/utils/",
+	"\0"
 };
